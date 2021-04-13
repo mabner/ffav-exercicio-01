189204c96d76d717934501249a71c34a052e6e10
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-12 23:27:23 -0300

    header implemented

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main">
+      <Header class="header" title="FFAV - Exercício 1" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="header">
-
-    </div>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
 </template>
 
 <script>
 export default {
-
+    name: "Header",
+    props: {
+        title: String
+    }
 }
 </script>
 
